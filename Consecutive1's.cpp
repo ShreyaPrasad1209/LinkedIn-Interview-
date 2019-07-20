7f5bcddc09cb8552c355cfd648ddb4c727322180
Shreya Prasad <shreyapd06@gmail.com>
2019-07-21 00:47:52 +0530

    Create Consecutive1's.cpp
    Given a base-10 integer, n, convert it to binary (base-2). Then find and print the base-10 integer denoting the maximum number of consecutive 1's in n's binary representation.

---
+++ Consecutive1's.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxConsecutiveOnes(int x) 
+{ 
+ 
+    int count = 0; 
+  
+
+    while (x!=0) 
+    { 
+
+        x = (x & (x << 1)); 
+  
+        count++; 
+    } 
+  
+    return count; 
+}
+int main()
+{
+    int n,k;
+    cin >> n;
+    cout << maxConsecutiveOnes(n);
+}
